540bd862c3d43f01ef1565a500917b3838e66d32
CRjulianTICO <crjuliantico@gmail.com>
2019-04-06 08:29:36 -0600

    Creation Project

---
+++ SIRERH/SIRERH/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SIRERH.MvcApplication" Language="C#" %>
---
+++ SIRERH/SIRERH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SIRERH.Startup))]
+namespace SIRERH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
